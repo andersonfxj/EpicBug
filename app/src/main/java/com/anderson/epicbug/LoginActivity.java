b28e62c41c9d7b0ee59ea7ab868b817ccbc81f6b
andersonfxj <andersoxfx.moreira@gmail.com>
2020-12-07 21:59:50 -0300

    Login

--- app/src/main/java/com/anderson/epicbug/LoginActivity.java
+++ app/src/main/java/com/anderson/epicbug/LoginActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
