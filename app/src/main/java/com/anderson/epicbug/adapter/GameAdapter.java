dfbd9b630305cde839043aa9c3a8aa278a26dbb3
killerlawliet <anderson_07_06@hotmail.com>
2020-12-09 21:46:43 -0400

    orielguilherme 09/12/2020

--- app/src/main/java/com/anderson/epicbug/adapter/GameAdapter.java
+++ app/src/main/java/com/anderson/epicbug/adapter/GameAdapter.java
@@ -1,6 +1,74 @@
 package com.anderson.epicbug.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class GameAdapter extends RecyclerView.Adapter {
+import com.anderson.epicbug.R;
+import com.anderson.epicbug.model.Jogo;
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * 1º Implementar o ViewHolder
+ * 2º Implementar o construtor da V.H
+ * 3º Terminar de Extender o adapter
+ * 4 º Implementar o construtor do adapter
+ * 5º Implementar os métodos do adapter
+ * */
+
+//View Holder
+public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
+    private Context context;
+    private ArrayList<Jogo> listaJogo;
+
+    public GameAdapter(Context c, ArrayList<Jogo> l){
+        context = context;
+        listaJogo = new ArrayList<Jogo>(l);
+    }
+
+    @NonNull
+    @Override
+    public GameViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        /* Convertendo o XML -> View */
+        View v = LayoutInflater.from(context).inflate(R.layout.item_game,parent, false);
+        return new GameViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GameViewHolder holder, int position) {
+        Jogo jogo = listaJogo.get(position);
+        holder.nomeJogo.setText(jogo.getNomeJogo());
+        holder.precoJogo.setText(jogo.getPrecoJogo().toString());
+        Glide.with(context).load(jogo.getUriImagem()).into(holder.uriImagem);
+       }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class GameViewHolder extends RecyclerView.ViewHolder{
+        //atributos
+        CircleImageView uriImagem ;
+        TextView nomeJogo;
+        TextView precoJogo;
+
+        //implementando a V.h
+        public GameViewHolder(@NonNull View itemView) {
+            super(itemView);
+            uriImagem = itemView.findViewById(R.id.imagemUri);
+            nomeJogo = itemView.findViewById(R.id.nomeJogo);
+            precoJogo = itemView.findViewById(R.id.precoJogo);
+        }
+
+    }
 }
---
+++ app/src/main/java/com/anderson/epicbug/model/Jogo.java
@@ -0,0 +1,54 @@
+package com.anderson.epicbug.model;
+
+//classe jogo
+public class Jogo {
+    private String id;
+    private String uriImagem;
+    private String nomeJogo;
+    private Double precoJogo;
+
+    //constructor vazio
+    public Jogo(){
+    }
+
+    //Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUriImagem(){
+        return uriImagem;
+    }
+
+    public void setUriImagem(String uriImagem){
+        this.uriImagem = uriImagem;
+    }
+
+    public String getNomeJogo(){
+        return nomeJogo;
+    }
+
+    public void setNomeJogo(String nomeJogo){
+        this.nomeJogo = nomeJogo;
+    }
+
+   public Double getPrecoJogo(){
+        return precoJogo;
+   }
+
+    public void setPrecoJogo(Double precoJogo){
+        this.precoJogo = precoJogo;
+    }
+
+    public Jogo(String id, String uriImagem, String nomeJogo, Double precoJogo){
+        this.id = id;
+        this.uriImagem = uriImagem;
+        this.nomeJogo = nomeJogo;
+        this.precoJogo = precoJogo;
+    }
+}
