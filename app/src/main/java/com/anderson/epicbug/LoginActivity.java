c8b006fa8c97048c92929fa612689fa42798ab40
andersonfxj <andersoxfx.moreira@gmail.com>
2020-12-09 22:50:15 -0300

    EpicBug tela

--- app/src/main/java/com/anderson/epicbug/LoginActivity.java
+++ app/src/main/java/com/anderson/epicbug/LoginActivity.java
@@ -2,7 +2,9 @@ package com.anderson.epicbug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void AbrirTelaCadastro(View view){
+        Intent intent = new Intent(LoginActivity.this,CadastroActivity.class);
+        startActivity(intent);
     }
 }
